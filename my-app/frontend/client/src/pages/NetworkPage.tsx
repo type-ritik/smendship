7237b92f068dcb0fca0ee61e11c3ef1b3411d513
rksvw <ritik.vw98@gmail.com>
2026-04-14 20:38:10 +0530

    feat: update NetworkPage styling and layout for improved invitation display

--- my-app/frontend/client/src/pages/NetworkPage.tsx
+++ my-app/frontend/client/src/pages/NetworkPage.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 function NetworkPage() {
@@ -7,16 +6,19 @@ function NetworkPage() {
   return (
     <div className="w-full flex justify-center h-full">
       <div className="flex flex-col justify-center items-center bg-white h-full p-5! rounded-xl">
-        <h1 className="text-2xl font-bold">Network Page</h1>
+        {/* Invitation */}
         <div className="invitation">
-          <div className="flex items-center gap-2 w-250 h-30 border border-gray-400 rounded-sm p-5! hover:shadow shadow-2xs shadow-black">
+          <div className="flex bg-sky-200
+          items-center gap-2 w-250 h-25 border border-gray-400 rounded-sm p-5! hover:shadow shadow-2xs shadow-black not-md:w-150">
             <div
               className="w-20 cursor-pointer"
               onClick={() => {
                 navigate(`/network/invitaion/${id}`);
               }}
             >
-              <div className="w-16 h-16 overflow-hidden rounded-full">
+              <div className="w-14 h-14 overflow-hidden rounded-full
+              border
+              ">
                 <img
                   src="https://media.istockphoto.com/id/1413735503/photo/social-media-social-media-marketing-thailand-social-media-engagement-post-structure.jpg?s=2048x2048&w=is&k=20&c=i6oJR_ptHv1Yf6NeZ6ncHpS-OX04GUotOhB2Mug20n4="
                   alt="Profile-Picture"
@@ -30,19 +32,30 @@ function NetworkPage() {
                 navigate(`/network/invitaion/${id}`);
               }}
             >
-              <h3 className="text-xl font-semibold text-gray-800">
+              <h3 className="text-lg font-semibold text-gray-800">
                 Ritik Sharma
               </h3>
-              <p className="text-sm text-gray-600">Software Engineer</p>
-              <span className="time">
+              <p className="text-[14px] text-gray-600">Software Engineer</p>
+              <span className="time text-[12px]">
                 <span className="text-gray-500">2h</span> &#8226;{" "}
               </span>
             </div>
             <div className="flex gap-5 w-1/2 justify-end-safe">
-              <button className="border py-3! px-6! bg-gray-200! text-red-700!  rounded justify-center flex flex-1 text-sm font-medium uppercase">
+              <button
+                className="border py-3! px-6! bg-gray-200! text-red-700!  rounded justify-center flex flex-1 text-sm font-medium uppercase hover:bg-red-700! hover:text-gray-200!
+              transition-colors
+              shadow-sm hover:shadow-black shadow-red-700
+              "
+              >
                 Reject
               </button>
-              <button className="border py-3! px-6! rounded bg-gray-200! text-green-700! justify-center flex flex-1 text-sm font-medium uppercase">
+              <button
+                className="border py-3! px-6! rounded bg-gray-200! text-green-700! justify-center flex flex-1 text-sm font-medium uppercase
+              hover:bg-green-700! hover:text-gray-200!
+              transition-colors
+              shadow-sm hover:shadow-black shadow-green-700
+              "
+              >
                 Accept
               </button>
             </div>
