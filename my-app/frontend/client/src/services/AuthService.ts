efbdeb9d9baafe230ef4cf85d5bb6c860f5a744f
rksvw <ritik.vw98@gmail.com>
2026-06-23 14:50:54 +0530

    feat: add OTP verification component and update signup process to include email storage

--- my-app/frontend/client/src/services/AuthService.ts
+++ my-app/frontend/client/src/services/AuthService.ts
@@ -16,12 +16,38 @@ export const LOGIN_NOW = gql`
 export const SIGNUP_NOW = gql`
   mutation Signup($name: String!, $email: String!, $password: String!) {
     signup(name: $name, email: $email, password: $password) {
+      status
+      message
+    }
+  }
+`;
+
+export const OTP_VERIFICATION = gql`
+  mutation VerifyAccount($email: String!, $OTP: String!) {
+    verifyAccount(email: $email, OTP: $OTP) {
+      status
+      message
+      token
       user {
         id
         name
         email
       }
-      token
+    }
+  }
+`;
+
+export const ALTER_PROFILE_DATA = gql`
+  mutation Updateprofile($input: UpdateUserInput) {
+    updateprofile(input: $input) {
+      message
+      status
+      user {
+        id
+        name
+        email
+      }
+      lastUpdated
     }
   }
 `;
