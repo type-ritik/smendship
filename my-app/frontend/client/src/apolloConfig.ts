82624cedbb77fea2f7b066a741af753d4c588c19
rksvw <ritik.vw98@gmail.com>
2026-07-06 20:16:11 +0530

    feat: add graphql-ws support for subscriptions and update Apollo client configuration

--- my-app/frontend/client/src/apolloConfig.ts
+++ my-app/frontend/client/src/apolloConfig.ts
@@ -1,24 +1,47 @@
-import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
-// import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { ApolloClient, HttpLink, InMemoryCache, split } from "@apollo/client";
+import { RetryLink } from "@apollo/client/link/retry";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { getMainDefinition } from "@apollo/client/utilities";
+import { createClient } from "graphql-ws";
 
 // Http link for queries and mutation
 const httpLink = new HttpLink({
   uri: "http://localhost:4000/graphql",
   headers: {
-    authorization: window.localStorage.getItem("token")
-      ? `${window.localStorage.getItem("token")}`
-      : "",
+    authorization: window.localStorage.getItem("token") || "",
   },
 });
 
-// const wsLink = new GraphQLWsLink({
-//   uri: "ws://localhost:4000/graphql",
-//   options: {
-//     reconnect: true,
-//   },
-// });
+const retryLink = new RetryLink();
+
+const wsLnk = new GraphQLWsLink(
+  createClient({
+    url: "ws://localhost:4000/graphql",
+    connectionParams: () => ({
+      authorization: window.localStorage.getItem("token") || "",
+    }),
+  }),
+);
+
+const linkChain = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLnk,
+  retryLink.concat(httpLink),
+);
 
 export const client = new ApolloClient({
-  link: httpLink,
+  link: linkChain,
   cache: new InMemoryCache(),
+
+  clientAwareness: {
+    name: "react-web-client",
+    version: "1.0",
+  },
+  queryDeduplication: false,
 });
